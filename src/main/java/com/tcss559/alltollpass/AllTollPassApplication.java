f55911c4798ba0d792ca12adf0f03536cb2ef249
Sasir Pentyala <sasir.pentyala@target.com>
2022-12-07 15:14:50 +0530

    added cors

--- src/main/java/com/tcss559/alltollpass/AllTollPassApplication.java
+++ src/main/java/com/tcss559/alltollpass/AllTollPassApplication.java
@@ -2,7 +2,10 @@ package com.tcss559.alltollpass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author sikha
@@ -14,6 +17,17 @@ public class AllTollPassApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AllTollPassApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
